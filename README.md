59f6e6c8d5ca5c523727e5fbc1e2492828b08c28
reemayasrah <Rymkh1994@gmail.com>
2021-03-30 21:24:13 +0300

    modified JS,css,html code

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # Aboutme-game
 
-## hello, this is trial page for my lab-02 project.
+## This website has imformation about me
 
-My name is Reem Bani Ali, Biomedical engineer graduated from Jordan University of Science and Technology (JUST). My working experience was Call Center Representative (CSR) for one year at Extensya as my first job, a field researcher for International Labor Organization (ILO) and Call Center Representative (CSR) at Sprii.com respectively.
+Authour: Reem Bani Ali
+Main major: Biomedical engineer
+My age: 27 years old
 
-This code written by me with the TA's instructions.
+I graduated from Jordan University of Science and Technology (JUST).
+ My working experience was Call Center Representative (CSR) for one year at Extensya as my first job, a field researcher for International Labor Organization (ILO) and Call Center Representative (CSR) at Sprii.com respectively.
 
-HTML,CSS,and JS are used to creat it.
+I used HTML,CSS,and JS are to creat this website..
+
+ used loops, if statement,swutch in this code.
 
 The license for the code is (MIT).
--- js/app.js
+++ js/app.js
@@ -1,41 +1,46 @@
 'use strict';
 
+
 let userName = prompt('what is your name ?');
 //console.log(userName);
 alert('welcom ' + userName + ' we are happy to have you here :)');
-
+let score='';
 let traveller = prompt(' Do you think I am traveling right now?');
 traveller = traveller.toLowerCase();
 //console.log(traveller);
+// eslint-disable-next-line no-constant-condition
 if (traveller == 'yes' || 'y') {
-    alert('you are wrong');
-    //console.log(traveller);
+  alert('you are wrong');
+  //console.log(traveller);
 }
-else if (traveller=='no'||'n') {
+// eslint-disable-next-line no-constant-condition
+else if (traveller == 'no' || 'n') {
 
-    alert(' You are right');
-    //console.log(traveller);
+  alert(' You are right');
+  score++;
+  //console.log(traveller);
 
-}else{
-    alert('please write yes/y or no/n');
+} else {
+  alert('please write yes/y or no/n');
 }
 
 
-let loveTraveling = prompt(' do think I like TO travel?');
+let loveTraveling = prompt(' do think I like to travel?');
 loveTraveling = loveTraveling.toLowerCase();
 // console.log(loveTraveling);
 switch (loveTraveling) {
-    case loveTraveling = 'yes':
-    case loveTraveling = 'y':
-        alert('correct');
-        break;
-    case loveTraveling = 'no':
-    case loveTraveling = 'n':
-        // console.log(loveTraveling);
-        alert('this is not true! ');
-        break;
-        default:
-        alert('please write yes/y or no/n');
+case loveTraveling = 'yes':
+case loveTraveling = 'y':
+  alert('correct');
+  score++;
+  break;
+case loveTraveling = 'no':
+case loveTraveling = 'n':
+  // console.log(loveTraveling);
+  alert('this is not true! ');
+  break;
+default:
+  alert('please write yes/y or no/n');
 
 }
 
@@ -44,18 +49,19 @@ travelingNumber = travelingNumber.toLowerCase();
 //console.log(travelingNumber);
 
 switch (travelingNumber) {
-    case travelingNumber = 'yes':
-    case travelingNumber = 'y':
-        //console.log(travelingNumber);
-        alert('I hope, but this is not the right answer!');
-        break;
-    case travelingNumber = 'no':
-    case travelingNumber = 'n':
-        //console.log(travelingNumber);
-        alert('this is correct');
-        break;
-    default:
-        alert('please write yes/y or no/n');
+case travelingNumber = 'yes':
+case travelingNumber = 'y':
+  //console.log(travelingNumber);
+  alert('I hope, but this is not the right answer!');
+  break;
+case travelingNumber = 'no':
+case travelingNumber = 'n':
+  //console.log(travelingNumber);
+  alert('this is correct');
+  score++;
+  break;
+default:
+  alert('please write yes/y or no/n');
 }
 
 let distination = prompt(' Was USA  my last disination?');
@@ -63,19 +69,20 @@ distination = distination.toLowerCase();
 //console.log(distination);
 
 switch (distination) {
-    case distination = 'yes':
-    case distination = 'y':
-        // console.log(distination);
-        alert('wrong, but it is intersting country!');
-        break;
+case distination = 'yes':
+case distination = 'y':
+  // console.log(distination);
+  alert('wrong, but it is intersting country!');
+  break;
 
-    case distination = 'no':
-    case distination = 'n':
-        //console.log(distination);
-        alert(' You are right:(');
-        break;
-        default:
-        alert('please write yes/y or no/n');
+case distination = 'no':
+case distination = 'n':
+  //console.log(distination);
+  alert(' You are right:(');
+  score++;
+  break;
+default:
+  alert('please write yes/y or no/n');
 }
 
 let cost = prompt(' Do you think my last trip coast me more than $8000?');
@@ -83,21 +90,70 @@ cost = cost.toLowerCase();
 //console.log(cost);
 
 switch (cost) {
-    case cost = 'yes':
-    case cost = 'y':
-        //console.log(cost);
-        alert('correct, traveling can be expensive sometimes ');
-        break;
+case cost = 'yes':
+case cost = 'y':
+  //console.log(cost);
+  alert('wrong');
+  break;
 
-    case cost = 'no':
-    case cost = 'n':
-        //console.log(cost);
-        alert('true, It cost me less');
-        break;
-        default:
-        alert('please write yes/y or no/n');
+case cost = 'no':
+case cost = 'n':
+  //console.log(cost);
+  alert('true, It cost me less');
+  score++;
+  break;
+default:
+  alert('please write yes/y or no/n');
 }
+
+ let numericGuess = prompt(' Guess my age - it is betwwen 20 and 30 ?');
+//console.log(traveller);
+
+for (let i=0;i<3;i++){if (numericGuess > 27) {
+  alert('  too high');
+  numericGuess = prompt(' Guess my age - it is betwwen 20 and 30 ?');
+
+
+}else if (numericGuess <27 ){
+  alert('  too low');numericGuess = prompt(' Guess my age - it is betwwen 20 and 30 ?');}
+else if (numericGuess==27) {
+  alert('correct');
+  score++;
+  break;
+}
+}
+alert('It is a gueesing Game, you have 6 trials to guess my favorit resturent?');
+
+
+
+let userAnswer = false;
+let favRst = ['qaiser', 'mac', 'abu-shekha', 'daiaa'];
+for (let i = 0; i < 6; i++) {
+    let rstQ = prompt('what is  my favourit resturint?');
+
+    for (let j = 0; j < favRst.length; j++) {
+
+
+        if (rstQ == favRst[j]) {
+            alert(' you guessed correctly');
+            userAnswer = true;
+            //   score++;
+            break;
+        }
+
+    } if (userAnswer == true) {
+        break;
+    }
+    else if (i == 5) {
+        alert('these are my favourite resturents  ' + favRst);
+    }
+    else if (userAnswer == false) {
+        alert('try again');
+    }
+
+}
+
 
 confirm('you are above 18?');
 
-alert('Thank you ' + userName + ' Hope you have great time');
+alert('Thank you ' + userName + ' Hope you have great time'+'your score is '+score );
